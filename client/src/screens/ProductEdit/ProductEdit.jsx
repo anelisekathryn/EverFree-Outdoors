6ff871f98b8a0f1e8aca746817ca216b427d1fcf
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-11-16 14:14:18 -0500

    productEdit and productDetail

--- client/src/screens/ProductEdit/ProductEdit.jsx
+++ client/src/screens/ProductEdit/ProductEdit.jsx
@@ -1,9 +1,34 @@
+import { useState,useEffect } from "react"
+import { useParams } from "react-router"
+import { Layout } from "../../components"
+import { getProduct } from "../../services/products"
 
 const ProductEdit = () => {
+  const [product, setProduct] = useState([])
+  let { id } = useParams()
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const product = await getProduct(id)
+      setProduct(product)
+      
+    }
+    
+    fetchProduct()
+  },[id])
   return (
+    <Layout>
     <div>
-      <h2>This will be the product edit page</h2>
-    </div>
+      <form htmlFor='EditInfo'>
+        <input type="text" placeholder={product.name}  required/>
+        <input type="text" placeholder={product.price}  required/>
+        <textarea placeholder={product.description} required></textarea>
+        <input type="text" placeholder='Image Link' required />
+        <button type='submit' className='save-button'>
+            Save
+          </button>
+      </form>
+      </div>
+      </Layout>
   )
 }
 
