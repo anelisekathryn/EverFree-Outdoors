24a87a5ec87894e3a2621c4ef9e5492c23883f55
Lance Simmons <lancehowardsimmons@gmail.com>
2021-11-16 15:31:46 -0500

    conditional link for add products in footer

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,12 +1,32 @@
 import './Nav.css'
 import { NavLink } from 'react-router-dom'
 
-const Nav = () => {
+const authenticatedOptions = (
+  <>
+      <NavLink className="link" to="/add-product">Add Product</NavLink>
+      <NavLink className="link" to="/sign-out">Sign Out</NavLink>
+  </>
+)
+const unauthenticatedOptions = (
+  <>
+      <NavLink className="link" to="/sign-up">Sign Up</NavLink>
+      <NavLink className="link" to="/sign-in">Sign In</NavLink>
+  </>
+)
+const alwaysOptions = (
+  <>
+      <NavLink className="link" to="/products">Products</NavLink>
+  </>
+)
+
+const Nav = ({ user }) => {
   return (
     <nav>
       <NavLink className='logo' to='/'>EverFree Outdoors</NavLink>
       <div className='links'>
-
+        {user && <div className="link welcome">Welcome, {user.username}</div>}
+        {alwaysOptions}
+        {user ? authenticatedOptions : unauthenticatedOptions}
       </div>
     </nav>
   )
