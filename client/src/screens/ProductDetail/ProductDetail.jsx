a2ec0e9ca2fbdedee466e803e883ad44e6737039
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-11-16 11:45:53 -0500

    productDetail api call

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -1,8 +1,25 @@
-
+import { useState, useEffect } from 'react'
+import { getProduct, deleteProduct } from '../../services/products';
+import { useParams, Link} from 'react-router-dom'
 const ProductDetail = () => {
+  const [product, setProduct] = useState([])
+  let { id } = useParams()
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const product = await getProduct(id)
+      setProduct(product)
+      
+    }
+    console.log(product)
+    fetchProduct()
+  },[id])
   return (
     <div>
       <h2>This will be the product detail page</h2>
+      <div className='name'>{product.name }</div>
+      <div className='price'>{`$${product.price}`}</div>
+      <img src={ product.imgURL }/>
+          <div className='description'>{product.description}</div>
     </div>
   )
 }
