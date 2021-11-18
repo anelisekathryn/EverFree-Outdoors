7c83c20d6279cee41e1207be8e1a2af60afeda35
Lance Simmons <lancehowardsimmons@gmail.com>
2021-11-18 10:41:55 -0500

    even spacing on non-home nav link items

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -36,7 +36,7 @@ const Nav = ({ user }) => {
       <NavLink
         className='logo'
         to='/'>
-        <img src={location.pathname === '/' ? "https://i.imgur.com/u3u8JHb.png" : "https://i.imgur.com/iIWZzLV.png"} />
+        <img src={location.pathname === '/' ? "https://i.imgur.com/M7GAgcy.png" : "https://i.imgur.com/iIWZzLV.png"} />
       </NavLink>
       
       <div className='links'>
